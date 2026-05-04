90d5f50d0780b06e9a46047d0c6eece3f0776031
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-04 13:50:06 +0200

    Mise en place du bind thow-way de parent vers enfant et vice versa avec EventCallback

---
+++ BlazorApp_Formation_Pizza/Components/Layout/TableWeather.razor.cs
@@ -0,0 +1,30 @@
+﻿using BlazorApp_Formation_Pizza_Model_DTO;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Formation_Pizza.Components.Layout
+{
+    /// <summary>
+    /// mise en place du bind two-way Parent vers enfant et vice versa
+    /// </summary>
+    public class TableWeatherBase : ComponentBase
+    {
+        #region parameter properties public
+        [Parameter]
+        public WeatherForecast[]? Forecasts { get; set; } = null;
+        #endregion
+
+        #region parameter properties 
+        [Parameter]
+        public EventCallback<WeatherForecast[]?> ForecastsChanged { get; set; }
+        #endregion
+
+
+        #region protected view method
+        public void OnClickClear () { 
+            Forecasts = null;
+            ForecastsChanged.InvokeAsync(Forecasts);
+        }
+
+        #endregion
+    }
+}
